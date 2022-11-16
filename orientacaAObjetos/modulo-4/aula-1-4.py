86383aa176190dada7ec6196725542bd8a644951
Felipe <felipeldnascimento@gmail.com>
2022-11-15 22:21:55 -0300

    Modulo 5 aula 1

--- orientacaAObjetos/modulo-4/aula-1-4.py
+++ orientacaAObjetos/modulo-4/aula-1-4.py
@@ -2,17 +2,16 @@ print("***************************************")
 print("*** Bem vindo ao jogo da advinhação ***")
 print("***************************************")
 
+#Variaveis
 numero_secreto = 42
 total_tentativas = 0
 limite_tentativas = 3
 
 while(total_tentativas < limite_tentativas):
-    print("Tentativa: ", total_tentativas, "de", limite_tentativas)
+    print("Tentativa: {} de {}".format(total_tentativas, limite_tentativas-2))
 
     chute = input("Digite um número: ")
-
     print("Você digitou: ", chute)
-
     chute = int(chute)
 
     acertou  = chute == numero_secreto
---
+++ orientacaAObjetos/modulo-5/for.py
@@ -0,0 +1,14 @@
+#Função range()
+#Parametro 1 - Inicio da contagem
+#Parametro 2 - Fim da contagem
+#Parametro 3 - Quantidade de saltos por interação
+
+animals = ['Cat',
+           'Leon',
+           'Bird',   
+           'Pig',
+           'Wolf',
+           'Hawk']
+
+for w in animals:
+    print(w, len(w))
